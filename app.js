81fa664724c226c2a492b3f2598643d150526642
Ellen Brøter <143997330+ellenbroter@users.noreply.github.com>
2023-10-13 12:25:16 +0200

    Change places for the buttons to work

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-// Button - Back to index
-const backButton = document.querySelector('.button-back');
-backButton.addEventListener('click', () =>{
-	window.history.back()
-});
-
 // Button - Back to the top
 const topButton = document.querySelector('.button-top');
 topButton.addEventListener('click', () =>{
@@ -14,11 +8,8 @@ topButton.addEventListener('click', () =>{
 	 });
 });
 
-const takeToTop = document.querySelector('.buttontop');
-takeToTop.addEventListener('click', () =>{
-	window.scroll({
-		top: 0, 
-		left: 0, 
-		behavior: 'smooth'
-	 });
+// Button - Back to index
+const backButton = document.querySelector('.button-back');
+backButton.addEventListener('click', () =>{
+	window.history.back()
 });
